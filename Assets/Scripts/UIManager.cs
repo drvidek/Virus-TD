4868f274ab9b334a972ec6c6978fc024e6cfb074
CoreyD177 <corey.david87@live.com>
2022-10-30 21:16:08 +1100

    Started Impltementation of UI and Input systems

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    #region Variables
+    public List<string> mobTypes = new List<string>();
+    public List<string> towerTypes = new List<string>();
+    public string[] currentHand = new string[8];
+    #endregion
+    #region Startup
+    private void Start()
+    {
+        
+    }
+    #endregion
+    #region Functions
+    public void UpdateDisplay()
+    {
+
+    }
+    public void UpdateHand()
+    {
+
+    }
+    #endregion
+}
